e9c33d6c097624ce6ee00b637b2c2f033e080f5c
isereb <ruxxay.coder@gmail.com>
2019-10-07 00:27:23 +0000

    init commit

---
+++ src/main/java/com/spring/integration/siexample/SiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.integration.siexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/integration/siexample/SiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.integration.siexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
